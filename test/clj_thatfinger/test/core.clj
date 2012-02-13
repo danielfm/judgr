45c82d1fc5ce0c8fa4c6aecd4accb4f3ba3b67cc
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2012-02-12 23:04:44 -0200

    Removed duplicated fixture data.

--- test/clj_thatfinger/test/core.clj
+++ test/clj_thatfinger/test/core.clj
@@ -6,26 +6,29 @@
 
 ;; Fixtures
 
-(def-fixture smoothing [factor classes]
+(def-fixture smoothing []
   (binding [clj-thatfinger.settings/*smoothing-enabled* true
-            clj-thatfinger.settings/*smoothing-factor* factor
-            clj-thatfinger.settings/*classes* classes]
+            clj-thatfinger.settings/*smoothing-factor* 1
+            clj-thatfinger.settings/*classes* '(:ok :offensive)]
     (test-body)))
 
 (def-fixture no-smoothing []
   (binding [clj-thatfinger.settings/*smoothing-enabled* false]
     (test-body)))
 
-(def-fixture test-db [& messages]
+(def-fixture test-db []
   (binding [clj-thatfinger.db.memory-db/*words* (atom {})
             clj-thatfinger.db.memory-db/*messages* (atom {})]
-    (doall (map #(apply add-message! %) messages))
+    (let [messages [["Você é um diabo, mesmo." :ok]
+                    ["Vai pro inferno, diabo!" :offensive]
+                    ["Sua filha é uma diaba, doido." :offensive]]]
+      (doall (map #(apply add-message! %) messages)))
     (test-body)))
 
 ;; Tests
 
 (deftest smoothing-factor-enabled
-  (with-fixture smoothing [1 '(:ok :offensive)]
+  (with-fixture smoothing []
     (testing "smoothing factor for a category"
       (is (= 1 (cat-factor))))
 
@@ -42,7 +45,7 @@
 
 (deftest probability-calculation
   (testing "with smoothing"
-    (with-fixture smoothing [1 '(:ok :offensive)]
+    (with-fixture smoothing []
       (is (= 4/102 (prob 3 100)))
       (is (= 1/102 (prob 0 100)))
       (is (= 1/102 (prob nil 100)))))
@@ -54,42 +57,32 @@
       (is (zero? (prob nil 100))))))
 
 (def class-probability
-  (with-fixture smoothing [1 '(:ok :offensive)]
-    (with-fixture test-db [["Você é um diabo, mesmo." :ok]
-                           ["Vai pro inferno, diabo!" :offensive]
-                           ["Sua filha é uma diaba, doido." :offensive]]
+  (with-fixture smoothing []
+    (with-fixture test-db []
       (is (= 2/5 (class-prob :ok)))
       (is (= 3/5 (class-prob :offensive))))))
 
 (deftest word-probability
-  (with-fixture smoothing [1 '(:ok :offensive)]
-    (with-fixture test-db [["Você é um diabo, mesmo." :ok]
-                           ["Vai pro inferno, diabo!" :offensive]
-                           ["Sua filha é uma diaba, doido." :offensive]]
+  (with-fixture smoothing []
+    (with-fixture test-db []
       (is (= 2/3 (word-prob "diab" :ok)))
       (is (= 3/4 (word-prob "diab" :offensive))))))
 
 (deftest word-class-probability
-  (with-fixture smoothing [1 '(:ok :offensive)]
-    (with-fixture test-db [["Você é um diabo, mesmo." :ok]
-                           ["Vai pro inferno, diabo!" :offensive]
-                           ["Sua filha é uma diaba, doido." :offensive]]
+  (with-fixture smoothing []
+    (with-fixture test-db []
       (is (= 16/43 (word-class-prob :ok "diab")))
       (is (= 27/43 (word-class-prob :offensive "diab"))))))
 
 (deftest message-class-probability
-  (with-fixture smoothing [1 '(:ok :offensive)]
-    (with-fixture test-db [["Você é um diabo, mesmo." :ok]
-                           ["Vai pro inferno, diabo!" :offensive]
-                           ["Sua filha é uma diaba, doido." :offensive]]
+  (with-fixture smoothing []
+    (with-fixture test-db []
       (is (= 2187/18275
              (message-class-prob "Você adora o diabo." :offensive))))))
 
 (deftest message-probabilities
-  (with-fixture smoothing [1 '(:ok :offensive)]
-    (with-fixture test-db [["Você é um diabo, mesmo." :ok]
-                           ["Vai pro inferno, diabo!" :offensive]
-                           ["Sua filha é uma diaba, doido." :offensive]]
+  (with-fixture smoothing []
+    (with-fixture test-db []
       (is (= {:offensive 19683/237575
               :ok 8192/237575}
              (message-probs "Você adora o diabo, filha."))))))
