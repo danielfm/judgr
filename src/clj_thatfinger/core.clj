f34175498b74ff643d2d45f67a40106ebb75a1fd
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2012-02-14 07:30:48 -0200

    Fixed code that computes weighted probability. Improved tests.

--- src/clj_thatfinger/core.clj
+++ src/clj_thatfinger/core.clj
@@ -11,48 +11,53 @@
     0))
 
 (defn total-factor
-  "Returns the smoothing factory for all classes."
-  []
+  "Returns the smoothing factor for the given class count."
+  [cls-count]
   (if *smoothing-enabled*
-    (* *smoothing-factor* (count *classes*))
+    (* *smoothing-factor* cls-count)
     0))
 
 (defn prob
-  "Returns the probability with smoothing, if it's enabled."
-  [count-cat count-total]
-  (/ (+ (if (nil? count-cat) 0 count-cat) (cat-factor))
-     (+ count-total (total-factor))))
+  "Returns the weighted probability, if smoothing is enabled."
+  [count-cat count-total cls-count]
+  (let [count-cat (+ (or count-cat 0) (cat-factor))
+        count-total (+ (or count-total 0) (total-factor cls-count))]
+    (/ count-cat count-total)))
 
 (defn prob-of-class
-  "Returns the probability of a message to be part of class cls."
+  "Returns the weighted probability of a message to be part of class cls."
   [cls]
-  (if *classes-unbiased*
-    (prob 1 (count *classes*))
-    (prob (count-messages cls) (count-messages))))
+  (let [cls-count (count *classes*)]
+    (if *classes-unbiased*
+      (prob 1 cls-count cls-count)
+      (prob (count-messages cls) (count-messages) cls-count))))
 
 (defn prob-of-word
-  "Returns the probability of a word to be part of a class cls."
+  "Returns the probability of a word given the message is classified as cls."
   [word cls]
   (let [w (get-word word)
+        cls-count (count-words)
         cls-prob (if-not (nil? w) (cls (:classes w)))]
-    (prob cls-prob (count-messages cls))))
+    (prob cls-prob (count-messages cls) cls-count)))
 
 (defn posterior-prob-of-word
-  "Returns the posterior probability that word appears in messages of class cls."
+  "Returns the probability of cls given word is present."
   [cls word]
   (let [prior (* (prob-of-word word cls) (prob-of-class cls))
         total-prob (reduce + (map #(* (prob-of-word word %)
                                       (prob-of-class %)) *classes*))]
-    (/ prior total-prob)))
+    (if (zero? total-prob)
+      0
+      (/ prior total-prob))))
 
 (defn posterior-prob-of-message
-  "Returns the posterior probability that message is classified as class cls."
+  "Returns the probability that message is classified as class cls."
   [message cls]
   (let [words (stem message)]
     (reduce * (map #(posterior-prob-of-word cls %) words))))
 
 (defn posterior-probs
-  "Returns the posterior probabilities of message for each possible class."
+  "Returns the probabilities of message for each possible class."
   [message]
   (zipmap *classes* (map #(posterior-prob-of-message message %) *classes*)))
 
--- test/clj_thatfinger/test/core.clj
+++ test/clj_thatfinger/test/core.clj
@@ -20,6 +20,7 @@
   (binding [clj-thatfinger.db.memory-db/*words* (atom {})
             clj-thatfinger.db.memory-db/*messages* (atom {})]
     (let [messages [["Você é um diabo, mesmo." :ok]
+                    ["Sai de ré, capeta." :offensive]
                     ["Vai pro inferno, diabo!" :offensive]
                     ["Sua filha é uma diaba, doido." :offensive]]]
       (doall (map #(apply add-message! %) messages)))
@@ -27,76 +28,89 @@
 
 ;; Tests
 
-(deftest smoothing-factor-enabled
-  (with-fixture smoothing []
-    (testing "smoothing factor for a category"
-      (is (= 1 (cat-factor))))
-
-    (testing "smoothing factor for all categories"
-      (is (= 2 (total-factor))))))
-
-(deftest smoothing-factor-disabled
-  (with-fixture no-smoothing []
-    (testing "smoothing factor for a category"
-      (is (zero? (cat-factor))))
-
-    (testing "smoothing factor for all categories"
-      (is (zero? (total-factor))))))
-
-(deftest probability-calculation
+(deftest cat-factor-fn
   (testing "with smoothing"
     (with-fixture smoothing []
-      (is (= 4/102 (prob 3 100)))
-      (is (= 1/102 (prob 0 100)))
-      (is (= 1/102 (prob nil 100)))))
+      (is (= 1 (cat-factor)))))
 
   (testing "without smoothing"
     (with-fixture no-smoothing []
-      (is (= 3/100 (prob 3 100)))
-      (is (zero? (prob 0 100)))
-      (is (zero? (prob nil 100))))))
+      (is (zero? (cat-factor))))))
+
+(deftest total-factor-fn
+  (testing "with smoothing"
+    (with-fixture smoothing []
+      (is (= 2 (total-factor 2)))))
+
+  (testing "without smoothing"
+    (with-fixture no-smoothing []
+      (is (zero? (total-factor 2))))))
+
+(deftest prob-fn
+  (testing "with smoothing"
+    (with-fixture smoothing []
+      (is (= 4/102 (prob 3 100 2)))
+      (is (= 1/102 (prob 0 100 2)))
+      (is (= 1/102 (prob nil 100 2)))))
+
+  (testing "without smoothing"
+    (with-fixture no-smoothing []
+      (is (= 3/100 (prob 3 100 2)))
+      (is (zero? (prob 0 100 2)))
+      (is (zero? (prob nil 100 2))))))
 
 (deftest prob-of-class-fn
-  (with-fixture smoothing []
-    (with-fixture test-db []
-      (testing "probability of :ok class"
-        (is (= 2/5 (prob-of-class :ok))))
+  (with-fixture test-db []
+    (testing "with smoothing"
+      (with-fixture smoothing []
+        (is (= 1/3 (prob-of-class :ok)))
+        (is (= 2/3 (prob-of-class :offensive)))))
 
-      (testing "probability of :offensive class"
-        (is (= 3/5 (prob-of-class :offensive)))))))
+    (testing "without smoothing"
+      (with-fixture no-smoothing []
+        (is (= 1/4 (prob-of-class :ok)))
+        (is (= 3/4 (prob-of-class :offensive)))))))
 
 (deftest prob-of-word-fn
-  (with-fixture smoothing []
-    (with-fixture test-db []
-      (testing "probability of word being :ok"
-        (is (= 2/3 (prob-of-word "diab" :ok))))
+  (with-fixture test-db []
+    (testing "with smoothing"
+      (with-fixture smoothing []
+        (is (= 1/6 (prob-of-word "diab" :ok)))
+        (is (= 3/14 (prob-of-word "diab" :offensive)))))
 
-      (testing "probability of word being :offensive"
-        (is (= 3/4 (prob-of-word "diab" :offensive)))))))
+    (testing "without smoothing"
+      (with-fixture no-smoothing []
+        (is (= 1 (prob-of-word "diab" :ok)))
+        (is (= 2/3 (prob-of-word "diab" :offensive)))))))
 
 (deftest posterior-prob-of-word-fn
-  (with-fixture smoothing []
-    (with-fixture test-db []
-      (testing "posterior probability of :ok given word"
-        (is (= 16/43 (posterior-prob-of-word :ok "diab"))))
+  (with-fixture test-db []
+    (testing "with smoothing"
+      (with-fixture smoothing []
+        (is (= 7/25 (posterior-prob-of-word :ok "diab")))
+        (is (= 18/25 (posterior-prob-of-word :offensive "diab")))))
 
-      (testing "posterior probability of :offensive given word"
-        (is (= 27/43 (posterior-prob-of-word :offensive "diab")))))))
+    (testing "without smoothing"
+      (with-fixture no-smoothing []
+        (is (= 1/3 (posterior-prob-of-word :ok "diab")))
+        (is (= 2/3 (posterior-prob-of-word :offensive "diab")))))))
 
 (deftest posterior-prob-of-message-fn
-  (with-fixture smoothing []
-    (with-fixture test-db []
-      (is (= 19683/237575
-             (posterior-prob-of-message "Você adora o diabo, filha." :offensive))))))
+  (with-fixture test-db []
+    (with-fixture smoothing []
+      (testing "probability of message being :offensive"
+        (is (= 31104/191425
+               (posterior-prob-of-message "Você adora o diabo, filha." :offensive)))))))
 
 (deftest posterior-probs-fn
-  (with-fixture smoothing []
-    (with-fixture test-db []
-      (is (= {:offensive 19683/237575
-              :ok 8192/237575}
-             (posterior-probs "Você adora o diabo, filha."))))))
+  (with-fixture test-db []
+    (with-fixture smoothing []
+      (testing "probabilities of message for each possible class"
+        (is (= {:offensive 31104/191425 :ok 2401/191425}
+               (posterior-probs "Você adora o diabo, filha.")))))))
 
 (deftest class-of-message-fn
-  (with-fixture smoothing []
-    (with-fixture test-db []
-      (is (= :offensive (class-of-message "Você adora o diabo, filha."))))))
+  (with-fixture test-db []
+    (with-fixture smoothing []
+      (testing "class with greatest probability without using a threshold"
+        (is (= :offensive (class-of-message "Você adora o diabo, filha.")))))))
