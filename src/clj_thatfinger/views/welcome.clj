f9ffbc00504fbd47fda2e08443fa61ad3c4fc0cd
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2012-02-15 22:31:36 -0200

    Removed unused files.

--- src/clj_thatfinger/views/welcome.clj
+++
@@ -1,9 +0,0 @@
-(ns clj-thatfinger.views.welcome
-  (:require [clj-thatfinger.views.common :as common]
-            [noir.content.getting-started])
-  (:use [noir.core :only [defpage]]
-        [hiccup.core :only [html]]))
-
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to ae"]))
