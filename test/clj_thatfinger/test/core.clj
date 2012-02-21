387f601679461c62d44424ce99968aba8270289b
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2012-02-21 11:29:46 -0200

    Fixing the fixture name.

--- test/clj_thatfinger/test/core.clj
+++ test/clj_thatfinger/test/core.clj
@@ -1,12 +1,11 @@
 (ns clj-thatfinger.test.core
   (:use [clj-thatfinger.core]
-        [clj-thatfinger.db.default-db]
-        [clojure.test]
+        [clj-thatfinger.test.fixtures]
         [clj-thatfinger.test.utils]
-        [clj-thatfinger.test.fixtures]))
+        [clojure.test]))
 
 (deftest prob-of-class-fn
-  (with-fixture test-db []
+  (with-fixture test-memory-db []
     (testing "with smoothing"
       (with-fixture smoothing [1]
         (is (float= 1/3 (prob-of-class :ok)))
@@ -18,7 +17,7 @@
         (is (float= 3/4 (prob-of-class :offensive)))))))
 
 (deftest prob-of-word-fn
-  (with-fixture test-db []
+  (with-fixture test-memory-db []
     (testing "with smoothing"
       (with-fixture smoothing [1]
         (is (float= 1/6 (prob-of-word "diab" :ok)))
@@ -36,7 +35,7 @@
           (is (zero? (prob-of-word "lombra" :offensive))))))))
 
 (deftest posterior-prob-of-word-fn
-  (with-fixture test-db []
+  (with-fixture test-memory-db []
     (testing "with smoothing"
       (with-fixture smoothing [1]
         (is (float= 7/25 (posterior-prob-of-word :ok "diab")))
@@ -48,14 +47,14 @@
         (is (float= 2/3 (posterior-prob-of-word :offensive "diab")))))))
 
 (deftest posterior-prob-of-message-fn
-  (with-fixture test-db []
+  (with-fixture test-memory-db []
     (with-fixture smoothing [1]
       (testing "probability of message being :offensive"
         (is (float= 4112702/4656612
                     (posterior-prob-of-message "Filha do diabo." :offensive)))))))
 
 (deftest posterior-probs-fn
-  (with-fixture test-db []
+  (with-fixture test-memory-db []
     (with-fixture smoothing [1]
       (testing "probabilities of message for each possible class"
         (let [probs (posterior-probs "Filha do diabo.")]
@@ -63,7 +62,7 @@
           (is (float= 11073190/46566128 (:ok probs))))))))
 
 (deftest classify-messages
-  (with-fixture test-db []
+  (with-fixture test-memory-db []
     (with-fixture smoothing [1]
       (testing "class with greatest probability"
         (is (= :offensive (classify "Filha do diabo."))))
