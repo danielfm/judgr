2ad32c3e51d51eb655b6dcd34b3e1607e7ebae1e
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2012-02-21 11:15:40 -0200

    Changed the test so that a dummy module is not needed.

--- src/clj_thatfinger/stemmer/dummy_stemmer.clj
+++
@@ -1,4 +0,0 @@
-(ns clj-thatfinger.stemmer.dummy_stemmer)
-
-(defn stem [s]
-  "dummy stemmer")
--- test/clj_thatfinger/test/stemmer/default_stemmer.clj
+++ test/clj_thatfinger/test/stemmer/default_stemmer.clj
@@ -2,12 +2,6 @@
   (:use [clj-thatfinger.stemmer.default-stemmer])
   (:use [clojure.test]))
 
-(defn dummy-module [f]
-  (binding [clj-thatfinger.settings/*stemmer-module* 'dummy_stemmer]
-    (f)))
-
-(use-fixtures :once dummy-module)
-
 (deftest module-require
   (testing "require module defined in settings"
-    (= "dummy stemmer" (stem "echo"))))
+    (= "brazilian-stemmer" (stemmer-module-name))))
