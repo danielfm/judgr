1cf4bd77068c85426eb3c3b27e96379682842ff6
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2012-02-25 11:07:39 -0200

    Introducing a global factory namespace that uses the settings module.

--- src/clj_thatfinger/extractor/factory.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-thatfinger.extractor.factory
-  (:import  [clj_thatfinger.extractor.brazilian_extractor BrazilianTextExtractor]))
-
-(defn make-brazilian-extractor
-  "Creates a new instance of BrazilianTextExtractor."
-  []
-  (BrazilianTextExtractor.))
---
+++ src/clj_thatfinger/factory.clj
@@ -0,0 +1,29 @@
+(ns clj-thatfinger.factory
+  (:require [clj-thatfinger.db [mongo-db :as mongo-db]]
+            [clj-thatfinger.extractor.brazilian-extractor])
+  (:import  [clj_thatfinger.db.memory_db MemoryDB]
+            [clj_thatfinger.db.mongo_db MongoDB]
+            [clj_thatfinger.extractor.brazilian_extractor BrazilianTextExtractor]))
+
+;;
+;; DBs
+;;
+(defmulti use-db #(-> % :database :type))
+
+(defmethod use-db :memory-db [settings]
+  (let [item-atom (atom '[])
+        feature-atom (atom {})]
+    (MemoryDB. settings item-atom feature-atom)))
+
+(defmethod use-db :mongo-db [settings]
+  (let [conn (mongo-db/create-connection! settings)]
+    (MongoDB. settings conn)))
+
+
+;;
+;; Extractors
+;;
+(defmulti use-extractor #(-> % :extractor :type))
+
+(defmethod use-extractor :brazilian-text-extractor [settings]
+  (BrazilianTextExtractor.))
