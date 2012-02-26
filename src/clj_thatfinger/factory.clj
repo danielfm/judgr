5b20c3d831f09fb9f734c8c13d9e480a811effd5
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2012-02-25 22:34:34 -0200

    Forgot to require the memory-db namespace.

--- src/clj_thatfinger/factory.clj
+++ src/clj_thatfinger/factory.clj
@@ -1,6 +1,9 @@
 (ns clj-thatfinger.factory
   (:require [clj-thatfinger.db [mongo-db :as mongo-db]]
-            [clj-thatfinger.extractor.brazilian-extractor])
+            [clj-thatfinger.db.memory-db]
+            [clj-thatfinger.extractor.brazilian-extractor]
+            [clj-thatfinger.classifier.default-classifier])
+
   (:import  [clj_thatfinger.db.memory_db MemoryDB]
             [clj_thatfinger.db.mongo_db MongoDB]
             [clj_thatfinger.extractor.brazilian_extractor BrazilianTextExtractor]))
