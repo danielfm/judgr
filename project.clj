eabc82686f331dd4809bd13ff90811ac158cbe3a
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2012-09-24 11:51:55 -0300

    Bumped version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject judgr "0.2.1"
+(defproject judgr "0.2.2"
   :description "Multi-class Naive Bayes Classifier library."
   :url "http://github.com/danielfm/judgr"
   :dependencies [[org.clojure/clojure "1.3.0"]
