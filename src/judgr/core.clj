b91667cf68c7ec6e1f32db5a004367b484394757
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2012-10-20 14:13:20 -0300

    Removed useless comments.

--- src/judgr/core.clj
+++ src/judgr/core.clj
@@ -9,35 +9,22 @@
             [judgr.extractor.english_extractor EnglishTextExtractor]
             [judgr.classifier.default_classifier DefaultClassifier]))
 
-;;
-;; DBs
-;;
 (defmulti db-from #(-> % :database :type))
+(defmulti extractor-from #(-> % :extractor :type))
+(defmulti classifier-from #(-> % :classifier :type))
 
 (defmethod db-from :memory [settings]
   (let [items (ref [])
         features (ref {})]
     (MemoryDB. settings items features)))
 
-
-;;
-;; Extractors
-;;
-(defmulti extractor-from #(-> % :extractor :type))
-
-(defmethod extractor-from :brazilian-text [settings]
-  (BrazilianTextExtractor.))
-
-(defmethod extractor-from :english-text [settings]
-  (EnglishTextExtractor.))
-
-
-;;
-;; Classifiers
-;;
-(defmulti classifier-from #(-> % :classifier :type))
-
 (defmethod classifier-from :default [settings]
   (let [db (db-from settings)
         extractor (extractor-from settings)]
     (DefaultClassifier. settings db extractor)))
+
+(defmethod extractor-from :brazilian-text [settings]
+  (BrazilianTextExtractor. settings))
+
+(defmethod extractor-from :english-text [settings]
+  (EnglishTextExtractor. settings))
