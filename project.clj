65d5680c37db98955896319a33cb2f2dc8dd0851
Daniel Fernandes Martins <daniel.fernandes@corp.globo.com>
2014-01-31 13:40:16 -0200

    Updated clojure version.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject judgr "0.3.1-SNAPSHOT"
   :description "Multi-class Naive Bayes Classifier library."
   :url "http://github.com/danielfm/judgr"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.macro "0.1.1"]
                  [org.apache.lucene/lucene-analyzers "3.5.0"]]
   :dev-dependencies [[lein-clojars "0.7.0"]])
